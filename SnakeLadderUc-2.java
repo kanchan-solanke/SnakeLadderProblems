a1768930125b539cd97a5147bcd20778157ca0c8
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-12 00:45:48 +0530

    your dice result is

---
+++ SnakeLadderUc-2.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Dice{
+    public static void main(String[] args){
+        int dice;
+        int roll;
+        dice= (int)(Math.random()*6)+1;
+
+roll = dice;
+         System.out.println("Your dice result is:"+ dice);
+         System.out.println("total roll is:"+ roll);
+
+     }
+         }
